73b09ddcec1b5d5acb408dd1281d95f66b6dce0e
yasserelmalouani <yasser1998@outlook.it>
2024-11-05 15:29:19 +0100

    feat: added new rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,21 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-native/all',
   ],
-  plugins: ['prettier', '@react-native'],
+  plugins: ['prettier', '@react-native', 'unused-imports'],
   rules: {
     'prettier/prettier': 'error',
+    'react-native/sort-styles': 'off',
+    'react-native/no-color-literals': 'off',
+    'unused-imports/no-unused-imports': 'error', // Rimuove gli import non utilizzati
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'react-native-gesture-handler',
+          },
+        ],
+      },
+    ],
   },
 };
