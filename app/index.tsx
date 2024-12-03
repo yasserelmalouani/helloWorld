aba49e4de5f58d42325edc527be6369b9d2f118f
yasserelmalouani <yasser1998@outlook.it>
2024-12-03 15:14:28 +0100

    feat: created carts screen with its fetch api

--- app/index.tsx
+++ app/index.tsx
@@ -1,153 +1,53 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { RowItemMolecule } from '@/components/molecules/rowItem/rowItem.molecule';
-import { Button, ListRenderItem, View, FlatList } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, ListRenderItem, Text, View } from 'react-native';
 import { styles } from '@/app/styles';
 
-interface Task {
-  completed: boolean;
+interface Product {
   id: number;
-  todo: string;
-  userId: number;
-}
-
-interface Result {
-  limit: number;
-  skip: number;
-  todos: Task[];
+  title: string;
+  price: number;
+  quantity: number;
   total: number;
+  discountPercentage: number;
+  discountedTotal: number;
+  thumbnail: string;
 }
 
-enum Filter {
-  ALL = 'ALL',
-  COMPLETED = 'COMPLETED',
-  NOT_COMPLETED = 'NOT_COMPLETED',
+interface Cart {
+  id: number;
+  products: Product[];
+  total: number;
+  discountedTotal: number;
+  userId: number;
+  totalProducts: number;
+  totalQuantity: number;
+}
+
+interface Response {
+  carts: Cart[];
+  total: number;
+  skip: number;
+  limit: number;
 }
 
 export default function Index() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [filter, setFilter] = useState<Filter>(Filter.ALL);
+  const [carts, setCarts] = useState<Cart[]>([]);
 
-  // ** DATA ** //
-  const filteredTodos = useMemo(() => {
-    if (filter === Filter.ALL) {
-      return tasks;
-    }
-    if (filter === Filter.COMPLETED) {
-      return tasks.filter((task) => task.completed);
-    }
-    return tasks.filter((task) => !task.completed);
-  }, [filter, tasks]);
-
-  // ** CALLBACKS  ** //
-  const setAll = () => {
-    setFilter(Filter.ALL);
-  };
-
-  const setCompleted = () => {
-    setFilter(Filter.COMPLETED);
-  };
-
-  const setNotCompleted = () => {
-    setFilter(Filter.NOT_COMPLETED);
-  };
-
-  const manageTask = useCallback(
-    (id: number) => {
-      const taskToEdit = tasks.find((task) => task.id === id);
-      if (!taskToEdit) {
-        return;
-      }
-      fetch(`https://dummyjson.com/todos/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          completed: !taskToEdit.completed,
-        }),
-      })
-        .then((res) => res.json())
-        .then((result) => {
-          setTasks((prevTasks) => {
-            return prevTasks.map((task) => {
-              if (task.id === result.id) {
-                return result;
-              }
-              return task;
-            });
-          });
-        });
-    },
-    [tasks]
-  );
-
-  const onDeleteTaskPress = useCallback((id: number) => {
-    fetch(`https://dummyjson.com/todos/${id}`, {
-      method: 'DELETE',
-    })
-      .then((res) => res.json())
-      .then(
-        (result: {
-          id: number;
-          todo: string;
-          completed: boolean;
-          userId: number;
-          isDeleted: boolean;
-          deletedOn: string;
-        }) => {
-          setTasks((prevState) => {
-            return prevState.filter((task) => {
-              if (result.isDeleted && result.id === task.id) {
-                return;
-              }
-              return task;
-            });
-          });
-        }
-      );
+  // ** CALLBACKS ** //
+  const renderItem = useCallback<ListRenderItem<Cart>>(({ item }) => {
+    return <Text>{item.id}</Text>;
   }, []);
 
-  // ** USE EFFECTS ** //
+  // ** USE EFFECT ** //
   useEffect(() => {
-    // save data on init screen cause empty deps
-    fetch('https://dummyjson.com/todos')
+    fetch('https://dummyjson.com/carts')
       .then((res) => res.json())
-      .then((result: Result) => setTasks(result.todos));
+      .then((response: Response) => setCarts(response.carts));
   }, []);
-
-  // ** UI  ** //
-  const renderItem: ListRenderItem<Task> = useCallback(
-    ({ item }) => {
-      return (
-        <RowItemMolecule
-          title={item.todo}
-          isCompleted={item.completed}
-          onPress={() => manageTask(item.id)}
-          onDelete={() => onDeleteTaskPress(item.id)}
-        />
-      );
-    },
-    [manageTask, onDeleteTaskPress]
-  );
 
   return (
     <View style={styles.container}>
-      {/* VIEW CONTAINER FOR FILTERS BUTTONS*/}
-      <View style={styles.filtersContainer}>
-        <View style={[styles.buttonStyle, filter === Filter.ALL ? styles.activeFilter : null]}>
-          <Button title={'ALL'} onPress={setAll} />
-        </View>
-        <View
-          style={[styles.buttonStyle, filter === Filter.COMPLETED ? styles.activeFilter : null]}>
-          <Button title={'Completed'} onPress={setCompleted} />
-        </View>
-        <View
-          style={[
-            styles.buttonStyle,
-            filter === Filter.NOT_COMPLETED ? styles.activeFilter : null,
-          ]}>
-          <Button title={'Not completed'} onPress={setNotCompleted} />
-        </View>
-      </View>
-      <FlatList showsVerticalScrollIndicator={false} data={filteredTodos} renderItem={renderItem} />
+      <FlatList showsVerticalScrollIndicator={false} data={carts} renderItem={renderItem} />
     </View>
   );
 }
--- app/styles.ts
+++ app/styles.ts
@@ -5,36 +5,4 @@ export const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
-  textInput: {
-    flex: 1,
-    height: 40,
-    borderColor: 'gray',
-    backgroundColor: '#f9f9f9',
-    borderWidth: 1,
-    marginBottom: 16,
-  },
-  emptyTextInput: {
-    flex: 1,
-    height: 40,
-    borderColor: 'red',
-    backgroundColor: '#f9f9f9',
-    borderWidth: 1,
-    marginBottom: 16,
-  },
-  textInputContainer: {
-    flexDirection: 'row',
-  },
-  filtersContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  buttonStyle: {
-    borderWidth: 1,
-    borderColor: 'rgb(51,184,196)',
-    padding: 8,
-    borderRadius: 10,
-  },
-  activeFilter: {
-    borderColor: 'red',
-  },
 });
